4058ae56037ce9c730a281bab94fc7dae0612501
noranhe <63988546+noranhe@users.noreply.github.com>
2022-05-30 10:19:27 +0800

    [Del] 删除用不到的代码

--- vnpy_portfoliostrategy/engine.py
+++ vnpy_portfoliostrategy/engine.py
@@ -211,8 +211,6 @@ class StrategyEngine(BaseEngine):
         vt_orderids = []
 
         for req in req_list:
-            req.reference = strategy.strategy_name      # Add strategy name as order reference
-
             vt_orderid = self.main_engine.send_order(
                 req, contract.gateway_name)
 
